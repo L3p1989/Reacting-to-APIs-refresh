f6971f646b60442abbb2c06b49274a2e094bde30
L3p1989 <phogan110@live.com>
2021-04-20 00:41:41 -0700

    set up a test card for Films

--- src/components/FilmCard.jsx
+++ src/components/FilmCard.jsx
@@ -2,18 +2,17 @@ import React from "react";
 
 const FilmCard = (props) => {
   return (
-    <div className="row">
-      <div className="card">
-        <img src="" className="card-img-top" />
+    <div className="col-4">
+      <div className="card shadow my-2">
         <div className="card-body">
-          <h5 className="card-title">Test Title</h5>
+          <h5 className="card-title">
+            <u>Test Title</u>
+          </h5>
           <p className="card-text">
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </p>
-          <a href="" className="btn btn-primary">
-            Test
-          </a>
+          <button className="btn btn-primary">Test</button>
         </div>
       </div>
     </div>
