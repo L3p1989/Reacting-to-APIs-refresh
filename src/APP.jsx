18624a5f92ae36675eea33af7521c231746d34b7
L3p1989 <phogan110@live.com>
2021-04-20 00:42:16 -0700

    assigned button logic for conditional renders based on state. imported FilmCard

--- src/APP.jsx
+++ src/APP.jsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState } from "react";
+import FilmCard from "./components/FilmCard";
 
 const App = () => {
-    return(
-        <div className="container">
-            <div className="row justify-content-center mt-3">
-                <h1 className="text-center"><u>Studio Ghibli</u></h1>
-            </div>
-            <div className="row justify-content-center mt-5">
-                <button className="btn btn-primary btn-lg mr-5">Films</button>
-                <button className="btn btn-primary btn-lg ml-5">People</button>
-            </div>
-        </div>
-    );
+  const [isFilm, setIsFilm] = useState(false);
+
+  const [isPeople, setIsPeople] = useState(false);
+
+  const renderFilms = () => {
+    setIsFilm(true);
+    setIsPeople(false);
+  };
+
+  const renderPeople = () => {
+    setIsPeople(true);
+    setIsFilm(false);
+  };
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-3">
+        <h1 className="text-center">
+          <u>Studio Ghibli</u>
+        </h1>
+      </div>
+      <div className="row justify-content-center mt-5">
+        <button className="btn btn-primary btn-lg mr-5" onClick={renderFilms}>
+          Films
+        </button>
+        <button className="btn btn-primary btn-lg ml-5" onClick={renderPeople}>
+          People
+        </button>
+      </div>
+      <div className="row justify-content-center mt-5">
+        {isFilm ? <FilmCard /> : null}
+        {isPeople ? <p>People test</p> : null}
+      </div>
+    </div>
+  );
 };
 
 export default App;
