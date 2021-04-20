7c587536a0c624f322ed3fcf684cc6043ac4982b
L3p1989 <phogan110@live.com>
2021-04-20 00:51:36 -0700

    set up props for functionality

--- src/components/FilmCard.jsx
+++ src/components/FilmCard.jsx
@@ -1,22 +1,17 @@
 import React from "react";
 
-const FilmCard = (props) => {
-  return (
-    <div className="col-4">
-      <div className="card shadow my-2">
-        <div className="card-body">
-          <h5 className="card-title">
-            <u>Test Title</u>
-          </h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <button className="btn btn-primary">Test</button>
+const FilmCard = props => {
+    return (
+        <div className="col-4">
+            <div className="card shadow my-2">
+                <div className="card-body">
+                    <h5 className="card-title"><u>{props.title}</u></h5>
+                    <p className="card-text">{props.text}</p>
+                    <button className="btn btn-primary">More Info</button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default FilmCard;
