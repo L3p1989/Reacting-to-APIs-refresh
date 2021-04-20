95992487381df61027f28c1eef3c8f564a76f825
L3p1989 <phogan110@live.com>
2021-04-20 01:39:06 -0700

    set up props, added prop values

--- src/components/PeopleCard.jsx
+++ src/components/PeopleCard.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const PeopleCard = () => {
+const PeopleCard = props => {
     return (
         <div className="col-4">
             <div className="card shadow">
-                <h5 className="card-title">Name: test</h5>
-                <p className="card-text">Gender: test</p>
-                <p className="card-text">Age: test</p>
-                <p className="card-text">Eye Color: test</p>
-                <p className="card-text">Hair Color: test</p>
+                <h5 className="card-title">Name: {props.name}</h5>
+                <p className="card-text">Gender: {props.gender}</p>
+                <p className="card-text">Age: {props.age}</p>
+                <p className="card-text">Eye Color: {props.eye}</p>
+                <p className="card-text">Hair Color: {props.hair}</p>
             </div>
         </div>
     );
