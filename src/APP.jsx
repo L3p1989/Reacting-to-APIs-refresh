66140eb66fe8d6b8c37ecc9938bcb22d4bc2a7f2
L3p1989 <phogan110@live.com>
2021-04-19 23:59:03 -0700

    set up App.jsx with buttons and styles

--- src/APP.jsx
+++ src/APP.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const App = () => {
     return(
         <div className="container">
-            <h1>Hello World!</h1>
+            <div className="row justify-content-center mt-3">
+                <h1 className="text-center"><u>Studio Ghibli</u></h1>
+            </div>
+            <div className="row justify-content-center mt-5">
+                <button className="btn btn-primary btn-lg mr-5">Films</button>
+                <button className="btn btn-primary btn-lg ml-5">People</button>
+            </div>
         </div>
     );
 };
