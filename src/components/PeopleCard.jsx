6dcffaadc4e4c82ee95f94842bc97c49cab12e34
L3p1989 <phogan110@live.com>
2021-04-20 01:36:18 -0700

    created PeopleCard with card that has test values

---
+++ src/components/PeopleCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const PeopleCard = () => {
+    return (
+        <div className="col-4">
+            <div className="card shadow">
+                <h5 className="card-title">Name: test</h5>
+                <p className="card-text">Gender: test</p>
+                <p className="card-text">Age: test</p>
+                <p className="card-text">Eye Color: test</p>
+                <p className="card-text">Hair Color: test</p>
+            </div>
+        </div>
+    );
+};
+
+export default PeopleCard;
